dbcea71a9b0209116954af733e91197d3cb4167d
danluong <danluong@outlook.com>
2015-05-04 17:14:49 -0700

    Completed Day4App
    Example of navigating between view controllers.

---
+++ Objective C/Day4App/Day4App/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  Day4App
+//
+//  Created by Dan Luong on 5/4/15.
+//  Copyright (c) 2015 Dan Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+- (IBAction)tappedCloseModal:(id)sender;
+
+@end
