039d1ae75be732436b3c17505af9c41bc0f4e125
danluong <danluong@outlook.com>
2015-05-12 14:22:21 -0700

    Completed Day25App
    Application lifecycle.

---
+++ Objective C/Day25App/Day25App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day25App
+//
+//  Created by Dan Luong on 5/12/15.
+//  Copyright (c) 2015 Dan Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective C/Day25App/Day25App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day25App
+//
+//  Created by Dan Luong on 5/12/15.
+//  Copyright (c) 2015 Dan Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
