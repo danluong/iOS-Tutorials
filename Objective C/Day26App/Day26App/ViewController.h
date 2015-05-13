09d725694248562092ac21f3b6f89efc47606580
danluong <danluong@outlook.com>
2015-05-13 11:52:23 -0700

    Completed Day26App
    Notifications using Parse instead of Azure

---
+++ Objective C/Day26App/Day26App/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Day26App
+//
+//  Created by Dan Luong on 5/12/15.
+//  Copyright (c) 2015 Dan Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblInfo;
+
+
+@end
+
---
+++ Objective C/Day26App/Day26App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Day26App
+//
+//  Created by Dan Luong on 5/12/15.
+//  Copyright (c) 2015 Dan Luong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
